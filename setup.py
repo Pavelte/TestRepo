8719ccd74fa2b96b9fc00d2bed6933329525474f
Pavelte <teperpavel@gmail.com>
2017-07-07 00:49:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 dist = setup(
     name='supervisor2',
-    packages=['test', 'test.tired'],
+    packages=['test', 'test.tired','test.tired.stam'],
     entry_points={
         'console_scripts': ['fuck=test.tired.stam:main']},
     
