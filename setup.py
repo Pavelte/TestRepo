57a62612aa663bea9c117e0acb76cb5ae047423f
Pavelte <teperpavel@gmail.com>
2017-07-07 00:56:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 dist = setup(
-    name='supervisor2',
+    name='234234',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['fuck=stam']},
