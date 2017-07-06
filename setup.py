0865333046c3fd4ef24612cd201478bd51e7f66d
Pavelte <teperpavel@gmail.com>
2017-07-06 23:33:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
-from setuptools import setup, find_packages
-
-
 dist = setup(
-    name='test',
-    description="A system for controlling process state under UNIX",
-    author="Chris McDonough",
-    author_email="chrism@plope.com",
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
     entry_points={
         'console_scripts': [
-            'test2 = test.test2:main'
+            'test3 = test.test2:main',
         ],
     },
 )
